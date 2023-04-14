0e902c37c9ab9b295d9445df1f4161e1d9d68160
0rigin-All <fabian.larwa.fl@gmail.com>
2023-04-14 09:10:58 +0200

    un peu de style de fait pas de mal

--- screens/listView.js
+++ screens/listView.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, View, Text, Switch } from 'react-native';
+import {Button, StyleSheet, View, Text, Switch } from 'react-native';
 import EventsList from '../components/EventList';
 import ExhibitorsList from '../components/ExhibitorList';
 
@@ -10,7 +10,10 @@ export default function ListView(props) {
   if (!isEnabled){
   return (
     <View style={styles.container}>  
-    <Text>Event / Exposant</Text>
+    <View style={styles.box}>
+        <Text style={styles.text1}>Evenement</Text>
+        <Text style={styles.text2}>Exposant</Text>
+        </View>
     <Switch
         trackColor={{false: 'red', true: 'red'}}
         thumbColor={isEnabled ? 'red' : 'red'}
@@ -26,7 +29,10 @@ export default function ListView(props) {
   else{
     return (
         <View style={styles.container}>  
-        <Text>Event / Exposant</Text>
+        <View style={styles.box}>
+        <Text style={styles.text3}>Evenement</Text>
+        <Text style={styles.text4}>Exposant</Text>
+        </View>
         <Switch
             trackColor={{false: 'red', true: 'red'}}
             thumbColor={isEnabled ? 'red' : 'red'}
@@ -49,5 +55,48 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
+  text3:{
+    color: 'white',
+    borderWidth: 3,
+    padding: 10,
+    borderBottomLeftRadius:20,
+    borderTopLeftRadius:20,
+    backgroundColor: 'red',
+    fontWeight: 'bold',
+    margin: -1
+  },
+  text4:{
+    color: 'red',
+    borderWidth: 3,
+    padding: 10,
+    borderBottomRightRadius:20,
+    borderTopRightRadius:20,
+    backgroundColor: 'white',
+    fontWeight: 'bold',
+    margin: -1
+  },
+  text1:{
+    color: 'red',
+    borderWidth: 3,
+    padding: 10,
+    borderBottomLeftRadius:20,
+    borderTopLeftRadius:20,
+    backgroundColor: 'white',
+    fontWeight: 'bold',
+    margin: -1
+  },
+  text2:{
+    color: 'white',
+    borderWidth: 3,
+    padding: 10,
+    borderBottomRightRadius:20,
+    borderTopRightRadius:20,
+    backgroundColor: 'red',
+    fontWeight: 'bold',
+    margin: -1
+  },
+  box:{
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
 });
